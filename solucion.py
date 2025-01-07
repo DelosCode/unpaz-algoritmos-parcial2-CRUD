682771a4beafae347c02a3585b3939e895c017f3
Angel G300 Saucedo <saucedog926@gmail.com>
2025-01-06 23:00:14 -0300

    template

---
+++ solucion.py
@@ -0,0 +1,17 @@
+class MiArchivo:
+    def __init__(self,nombre):
+        self.nombre = nombre
+    
+    def escribir(self, texto):
+        with open(self.nombre,"w") as archivo:
+            archivo.write(texto)
+    
+    def leer(self):
+        with open(self.nombre, "r") as archivo:
+            return archivo.read()
+
+mi_archivo = MiArchivo("mi_archivo.txt")
+
+mi_archivo.escribir("Hola, Estudiantes!")
+
+print(MiArchivo.leer())
